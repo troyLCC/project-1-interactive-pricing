2291b488b07224e59496b4eeeed4c54612ecc86e
troyLCC <timothyroy2011@gmail.com>
2021-09-12 08:57:02 +0600

    firs draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import './App.css';
+// import './App.css';
 import Header from "./components/header/Header"
 import Interactive_Pricing from './components/card/Interactive_Pricing';
 import Card from './components/card/Card'
+import styles from "./App.module.css"
 function App() {
   return (
-    <div>
+    <div className={styles.background1}>
+      <div className={styles.background2}></div>
       <Header/>
       <Card>
       <Interactive_Pricing /> 
